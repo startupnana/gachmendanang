5d34e49d7ab0d5d83ca2f32b22093c6efa29dad9
Nguyen Dac Kien <nguyendackien@LAP043.local>
2017-07-19 08:48:54 +0700

    add files

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Công Ty TNHH Một Thành Viên Nho Lộc</title>
+    <!-- Bootstrap core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Fontawesome core CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- custom CSS here -->
+    <link href="gachmen/style.css" rel="stylesheet">
+
+</head>
+<body class="back-section">
+    <div class="overlay">
+
+    
+ <div class="container">
+     <div class="row pad-top-one text-center">
+         <div class="col-lg-4 col-md-4 col-sm-4 ">
+            <span class="logo-cls">
+                <img src="gachmen/logo.png"/>
+            </span>
+         </div>
+         <div class="col-lg-2 col-md-2 col-sm-2 ">
+         </div>
+         <div class="col-lg-6 col-md-6 col-sm-6 ">
+             <i class="fa fa-home "></i> &nbsp;  &nbsp; Công Ty TNHH Một Thành Viên Nho Lộc
+             <br>
+             <i class="fa fa-phone"></i>&nbsp;  &nbsp;  0901.145.468 – 0905.955.486 (sale)
+             <br>
+             <i class="fa fa-mobile"></i>&nbsp;  &nbsp;  0905.8888.27 (hotline)
+             <br>
+             <i class="fa fa-envelope-o"></i>&nbsp;  &nbsp;  congdanhdng@gmail.com
+             <br>
+             Showroom : 347 Phạm Hùng,phường Hòa Xuân,Quận Cẩm Lệ,TP Đà Nẵng
+         </div>
+     </div>
+ </div>
+         <div class="col-lg-12 col-md-12 col-sm-12 head-line">
+            Sorry, Website <img src="gachmen/maintenance.png" class="img-circle img-custom"> Under Maintenance
+         </div>
+         <div class="col-lg-12 col-md-12 col-sm-12 sub-line">
+                           Please check after some time, till the time you can enjoy a movie or cup of tea
+
+         </div>
+    <div class="container">
+     <div class="row pad-top-two text-center">
+         <div class="col-lg-4 col-md-4 col-sm-4 social-div">
+             <a href="#"><i class="fa fa-facebook-square"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square"></i></a>
+              <a href="#"><i class="fa fa-twitter-square"></i></a>
+               <a href="#"><i class="fa fa-github-square"></i></a>
+               <br>
+            <a href="http://binarytheme.com/" style="color:#fff;font-size:16px;" target="_blank">By: gachmendanang.com</a>
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 ">
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 ">
+             Thôn Quang Châu,Xã Hòa Châu,huyện Hòa Vang TP Đà Nẵng.
+         </div>
+     </div>
+ </div>
+        </div>
+
+
+
+</body></html>
